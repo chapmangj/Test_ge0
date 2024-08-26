d167e5a1cfcfbf08dbe6125b3e7b0882df61acc8
chapmangj <gavinjchapman@gmail.com>
2024-08-26 20:06:31 +1000

    Update ge0.py

--- ge0.py
+++ ge0.py
@@ -49,4 +49,4 @@ if st.button("Get Information"):
     else:
         st.warning("Please enter a mineral deposit name.")
 
-st.write (" Oh, and I'm not as sharp as my human predecessor, so be sure to double-check anything important.")
+st.write (" Oh, and I'm not as sharp as my human predecessor, so be sure to double-check anything important.", font="12px")
